0b5bddde09534e54878c2b6ca905251af442476e
paprika <anastasias859@gmail.com>
2024-04-03 15:19:44 +0300

    changed http to https

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,6 @@
 const API = {
 	TOKEN: '5e964e0d91c230439d408c8c33c72cf083af5127',
-	LINK: 'http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio',
+	LINK: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio',
 };
 
 const defaultValue = {
